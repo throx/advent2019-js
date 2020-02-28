04205082bd2fd7d9d3b39f65e77eff4b6e8b1e38
John Wiltshire <jw@chase.net.au>
2020-02-28 00:31:06 +0000

    Day 1

---
+++ day1/main.js
@@ -0,0 +1,18 @@
+var fs = require("fs")
+var process = require("process")
+
+var nums = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(/\s/);
+
+const PART = 2;
+
+var totalFuel = 0;
+for (let num of nums) {
+    var mass = num;
+    do {
+        let fuel = Math.max(Math.floor(mass / 3) - 2, 0);
+        totalFuel += fuel;
+        mass = fuel;
+    } while (mass > 0 && PART == 2);
+}
+
+console.log(totalFuel);

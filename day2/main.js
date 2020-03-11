dc6b26e2f7672bdde092ed1a4576c383f9bd7742
John Wiltshire <jw@chase.net.au>
2020-03-11 05:21:35 +0000

    Day 2 Part 2

--- day2/main.js
+++ day2/main.js
@@ -1,9 +1,33 @@
 var fs = require("fs")
 var CPU = require("./cpu")
+var PART = 2
 
-var nums = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(/,/).map(Number);
-var proc = new CPU(nums);
+if (PART == 1) {
+    let nums = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(/,/).map(Number);
+    let proc = new CPU(nums);
 
-proc.run();
+    nums[1] = 12;
+    nums[2] = 1;
 
-console.log(proc.memory[0]);
+    proc.run();
+
+    console.log(proc.memory[0]);
+}
+else {
+    for (let noun = 0; noun <= 99; noun++) {
+        for (let verb = 0; verb <= 99; verb++) {
+            let nums = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(/,/).map(Number);
+            let proc = new CPU(nums);
+
+            nums[1] = noun;
+            nums[2] = verb;
+
+            proc.run();
+
+            if (proc.memory[0] == 19690720) {
+                console.log("Noun = " + noun + ", Verb = " + verb);
+                return;
+            }
+        }
+    }
+}

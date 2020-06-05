4f7d91f739eac5450471a8e9bb16b2f0d10224b2
John Wiltshire <jw@chase.net.au>
2020-06-05 15:44:07 +1000

    Add files via upload

--- day2/main.js
+++ day2/main.js
@@ -1,33 +1,36 @@
-var fs = require("fs")
-var CPU = require("./cpu")
-var PART = 2
+getFile("input.txt", function(data) {
 
-if (PART == 1) {
-    let nums = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(/,/).map(Number);
-    let proc = new CPU(nums);
-
-    nums[1] = 12;
-    nums[2] = 1;
-
-    proc.run();
-
-    console.log(proc.memory[0]);
-}
-else {
-    for (let noun = 0; noun <= 99; noun++) {
-        for (let verb = 0; verb <= 99; verb++) {
-            let nums = fs.readFileSync(__dirname + '/input.txt', 'utf8').split(/,/).map(Number);
-            let proc = new CPU(nums);
-
-            nums[1] = noun;
-            nums[2] = verb;
-
-            proc.run();
-
-            if (proc.memory[0] == 19690720) {
-                console.log("Noun = " + noun + ", Verb = " + verb);
-                return;
+    var PART = 2
+    
+    if (PART == 1) {
+        let nums = data.split(/,/).map(Number);
+        let proc = new CPU(nums);
+    
+        nums[1] = 12;
+        nums[2] = 1;
+    
+        proc.run();
+    
+        print(proc.memory[0]);
+    }
+    else {
+        for (let noun = 0; noun <= 99; noun++) {
+            for (let verb = 0; verb <= 99; verb++) {
+                let nums = data.split(/,/).map(Number);
+                let proc = new CPU(nums);
+    
+                nums[1] = noun;
+                nums[2] = verb;
+    
+                proc.run();
+    
+                if (proc.memory[0] == 19690720) {
+                    print("Noun = " + noun + ", Verb = " + verb);
+                    return;
+                }
             }
         }
     }
-}
+
+
+});
